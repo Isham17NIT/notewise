9b1a18843882d08a96e1012d92ef39b62632396d
Isham17NIT <ishamahuja17@gmail.com>
2025-07-06 23:37:40 +0530

    added edit note functionality on archive page and important page

--- src/pages/Archive/index.jsx
+++ src/pages/Archive/index.jsx
@@ -1,18 +1,67 @@
 import { Divider, IconButton } from "@mui/material"
 import { useDispatch, useSelector } from "react-redux"
+import {useReducer, useState} from 'react'
 import {v4 as uuid} from 'uuid'
 import UnarchiveIcon from '@mui/icons-material/Unarchive';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
-import { toggleArchive, addToBin } from "../../slices/notesSlice";
+import { toggleArchive, addToBin, editNote } from "../../slices/notesSlice";
 const Archive = ()=>{
     const dispatch = useDispatch();
     const archivedContents = useSelector(state=>{
        return state.notes.allNotes.filter((content)=>!content.isDeleted && content.isArchived)
     })
-    const editNote = ()=>{
-
+    const initialState = {
+        editId: '',
+        editTitle: '',
+        editDesc: ''
     }
+    const editReducer=(state,action)=>{
+        if(action.type==="ID_EDIT"){
+            return {
+                ...state,
+                editId: action.payload
+            }
+        }
+        else if(action.type==="TITLE_EDIT"){
+            return {
+                ...state,
+                editTitle: action.payload
+            }
+        }
+        else if(action.type==="DESC_EDIT"){
+            return {
+                ...state,
+                editDesc: action.payload
+            }
+        }
+        else if(action.type==="RESET"){
+            return initialState;
+        }
+        else{
+            return state;
+        }
+    }
+    const [state,editDispatch] = useReducer(editReducer,initialState);
+
+    const [isEditing, setIsEditing] = useState(false);
+
+    const saveEdit = ()=>{
+        dispatch(editNote({id:state.editId, title:state.editTitle, desc:state.editDesc}))
+        editDispatch({type:"RESET"})
+        setIsEditing(false)
+    }
+    const cancelEdit = ()=>{
+        editDispatch({type:"RESET"})
+        setIsEditing(false)
+    }
+    const startEditNote = (content)=>{
+        editDispatch({type:"TITLE_EDIT",payload:content.title})
+        editDispatch({type:"DESC_EDIT",payload:content.desc})
+        editDispatch({type:"ID_EDIT",payload:content.id})
+        setIsEditing(true)
+    }
+
     return (
         <div className="mt-[80px] w-full min-h-screen flex flex-col items-center">
             <div className="font-bold text-3xl text-center text-wrap">Archived Notes</div>
@@ -21,15 +70,56 @@ const Archive = ()=>{
                     archivedContents.map((content,idx)=>{
                         return (
                             <div key={content.id} className="flex flex-col p-4 min-h-52 max-h-52 rounded-2xl border-1 border-gray-200 w-full">
-                                <div className="truncate overflow-hidden whitespace-nowrap">{content.title}</div>
+                                {
+                                    state.editId===content.id && (
+                                        <textarea rows="1" value={state.editTitle}
+                                            className='p-1 w-full resize-none focus:outline-none focus:border-gray-200 
+                                                focus:border-[1px]' onChange={(e)=>editDispatch({type:"TITLE_EDIT",payload:e.target.value})}>
+                                        </textarea>
+                                    )
+                                }
+                                {
+                                    state.editId!==content.id && (
+                                        <div className="truncate overflow-hidden whitespace-nowrap">{content.title}</div>
+                                    )
+                                }
+
                                 <Divider/>
-                                <div className="overflow-y-auto max-h-30 break-words pre-wrap">{content.desc}</div>
+
+                                {
+                                    state.editId!==content.id && (
+                                        <div className="overflow-y-auto max-h-30 break-words pre-wrap w-full">{content.desc}</div>
+                                    )
+                                }
+                                {
+                                    state.editId===content.id && (
+                                        <textarea rows="4" value={state.editDesc}
+                                            className='p-1 w-full resize-none focus:outline-none focus:border-gray-200 
+                                                focus:border-[1px]' onChange={(e)=>editDispatch({type:"DESC_EDIT",payload:e.target.value})}>
+                                        </textarea>
+                                    )
+                                }
                                 <div className="flex-grow"></div>     
                                 <div className="flex">
                                     <div className="flex-grow"></div> 
-                                    <IconButton onClick={()=>dispatch(toggleArchive(content))}><UnarchiveIcon/></IconButton>  
-                                    <IconButton onClick={()=>dispatch(addToBin(content))}><DeleteOutlineIcon/></IconButton>   
-                                    <IconButton onClick={editNote}><EditOutlinedIcon/></IconButton>                                                     
+                                    {
+                                        state.editId==content.id && (
+                                            <div className="flex gap-1 justify-center items-center p-0.5">
+                                                <div className="p-1 hover:cursor-pointer hover:bg-gray-200 rounded-xl active:scale-95 text-center" onClick={saveEdit}>Save</div>
+                                                <div className="p-1 hover:cursor-pointer hover:bg-gray-200 rounded-xl active:scale-95 text-center" onClick={cancelEdit}>Cancel</div>
+                                            </div>
+                                        )
+                                    }
+                                    {
+                                        state.editId!==content.id && (
+                                            <div className="flex gap-1 justify-center items-center p-0.5">
+                                                <IconButton onClick={()=>dispatch(toggleArchive(content))}><UnarchiveIcon/></IconButton>  
+                                                <IconButton onClick={()=>dispatch(addToBin(content))}><DeleteOutlineIcon/></IconButton>   
+                                                <IconButton onClick={()=>startEditNote(content)}><EditOutlinedIcon/></IconButton> 
+                                            </div>
+                                        )
+                                    }
+                                                                                        
                                 </div>
                             </div>
                         )
--- src/pages/Important/index.jsx
+++ src/pages/Important/index.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
+import { useState, useReducer } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Divider, IconButton } from '@mui/material';
-import { addToBin, addToImportant } from '../../slices/notesSlice';
+import { addToBin, addToImportant,editNote } from '../../slices/notesSlice';
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
@@ -13,18 +13,69 @@ const Important = ()=>{
     const dispatch = useDispatch()
     const importantContents = useSelector(state=>state.notes.allNotes.filter((content)=>!content.isDeleted && content.isImportant))
     const createNote = ()=>{
-            if(title!=='' || desc!==''){
-                dispatch(addToImportant({id: uuid(), title: title, desc: desc}))
-                setTitle('')
-                setDesc('')
-            }
-            else{
-                alert("Empty note can't be created")
-            }
+        if(title!=='' || desc!==''){
+            dispatch(addToImportant({id: uuid(), title: title, desc: desc}))
+            setTitle('')
+            setDesc('')
+        }
+        else{
+            alert("Empty note can't be created")
+        }
     }
-    const editNote = ()=>{
 
+    const initialState = {
+        editId: '',
+        editTitle: '',
+        editDesc: ''
     }
+    const editReducer=(state,action)=>{
+        if(action.type==="ID_EDIT"){
+            return {
+                ...state,
+                editId: action.payload
+            }
+        }
+        else if(action.type==="TITLE_EDIT"){
+            return {
+                ...state,
+                editTitle: action.payload
+            }
+        }
+        else if(action.type==="DESC_EDIT"){
+            return {
+                ...state,
+                editDesc: action.payload
+            }
+        }
+        else if(action.type==="RESET"){
+            return initialState;
+        }
+        else{
+            return state;
+        }
+    }
+    const [state,editDispatch] = useReducer(editReducer,initialState);
+
+    const [isEditing, setIsEditing] = useState(false);
+
+    const saveEdit = ()=>{
+        dispatch(editNote({id:state.editId, title:state.editTitle, desc:state.editDesc}))
+        editDispatch({type:"RESET"})
+        setIsEditing(false)
+    }
+    const cancelEdit = ()=>{
+        editDispatch({type:"RESET"})
+        setIsEditing(false)
+    }
+    const startEditNote = (content)=>{
+        editDispatch({type:"TITLE_EDIT",payload:content.title})
+        editDispatch({type:"DESC_EDIT",payload:content.desc})
+        editDispatch({type:"ID_EDIT",payload:content.id})
+        setIsEditing(prev=>!prev)
+    }
+    
+    
+
     return (
         <div className="mt-[64px] w-full min-h-screen flex flex-col gap-4 items-center">
             <div className="font-bold text-3xl text-center text-wrap mt-2">Important Notes</div>
@@ -63,19 +114,54 @@ const Important = ()=>{
                             return (
                                 <div key={content.id} className="flex flex-col gap-0.5 items-center p-2 border-1
                                     border-gray-200 rounded-2xl w-full min-h-52 max-h-52">
-                                    <div className="flex w-full">
-                                        <div className="truncate overflow-hidden whitespace-nowrap">{content.title}</div>
-                                    </div>
+                                    {
+                                        state.editId===content.id && (
+                                            <textarea rows="1" value={state.editTitle}
+                                                className='p-1 w-full resize-none focus:outline-none focus:border-gray-200 
+                                                    focus:border-[1px]' onChange={(e)=>editDispatch({type:"TITLE_EDIT",payload:e.target.value})}>
+                                            </textarea>
+                                        )
+                                    }
+                                    {
+                                        state.editId!==content.id && (
+                                            <div className="truncate overflow-hidden whitespace-nowrap">{content.title}</div>
+                                        )
+                                    }
 
                                     <Divider className='w-full my-1'/>
 
-                                    <div className="overflow-y-auto max-h-30 break-words pre-wrap w-full">{content.desc}</div>
+                                    {
+                                        state.editId!==content.id && (
+                                            <div className="overflow-y-auto max-h-30 break-words pre-wrap w-full">{content.desc}</div>
+                                        )
+                                    }
+                                    {
+                                        state.editId===content.id && (
+                                            <textarea rows="4" value={state.editDesc}
+                                                className='p-1 w-full resize-none focus:outline-none focus:border-gray-200 
+                                                    focus:border-[1px]' onChange={(e)=>editDispatch({type:"DESC_EDIT",payload:e.target.value})}>
+                                            </textarea>
+                                        )
+                                    }
                                     <div className="flex-grow"></div>
                                     <div className="flex w-full">
-                                        <div>Important</div>
                                         <div className="flex-grow"></div>
-                                        <IconButton onClick={()=>dispatch(addToBin(content))}><DeleteOutlineOutlinedIcon/></IconButton>
-                                        <IconButton onClick={editNote}><EditOutlinedIcon/></IconButton>
+                                        {
+                                            state.editId!==content.id && (
+                                                <div className="flex gap-1 justify-center items-center p-0.5">
+                                                    <IconButton onClick={()=>dispatch(addToBin(content))}><DeleteOutlineOutlinedIcon/></IconButton>
+                                                    <IconButton onClick={()=>startEditNote(content)} disabled={isEditing}><EditOutlinedIcon/></IconButton>
+                                                </div>
+                                            )
+                                        }
+                                        {
+                                            state.editId===content.id && (
+                                                <div className="flex gap-1 justify-center items-center p-0.5">
+                                                    <div className="p-1 hover:cursor-pointer hover:bg-gray-200 rounded-xl active:scale-95 text-center" onClick={saveEdit}>Save</div>
+                                                    <div className="p-1 hover:cursor-pointer hover:bg-gray-200 rounded-xl active:scale-95 text-center" onClick={cancelEdit}>Cancel</div>
+                                                </div>
+                                            )
+                                        }
                                     </div>
                                 </div>
                             )
